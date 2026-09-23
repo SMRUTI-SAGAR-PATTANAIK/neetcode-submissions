ce1cf6696f133aa2002bc575c8119875a9768e97
SMRUTI-SAGAR-PATTANAIK <58901564+SMRUTI-SAGAR-PATTANAIK@users.noreply.github.com>
2026-09-23 19:03:38 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+        if(!head) return head;
+        let curr = head, next = head.next, prev = null;
+        while(curr) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
